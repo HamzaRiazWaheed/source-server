7cbe3df691dd2936fedd955248a913731bf0c411
Hamza Waheed <hamza.waheed@sanoma.com>
2020-01-09 23:50:23 +0200

    current weathe api working

--- routes/weather/index.js
+++ routes/weather/index.js
@@ -1,13 +1,39 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router();
+const fetch = require('node-fetch');
 
 // define the home page route
 router.get('/', function (req, res) {
-  res.send('Birds home page')
-})
+    const msg = `Get Weather Data`
+    res.send(msg)
+});
+
 // define the about route
-router.get('/about', function (req, res) {
-  res.send('About birds')
+router.get('/currentweather', function (req, res) {
+    let {
+        cords,
+        city
+    } = req.query;
+
+    let url = `${process.env.OPENWEATHER_URL}&units=metric`;
+    
+    if (cords) {
+        cords = cords.split(',');
+        url += '&lat=' + cords[0] + '&lon=' + cords[1];
+    }else if(city){
+        url += '&q=' + city;
+    }else {
+        res.send('Paramters Missing: cords=lat,lon&city=london,uk');
+    }
+    fetch(url).then(response => {
+            return response.json()
+        }).then(response => {
+            console.log(response);
+            res.send(response)
+        }).catch('error', (e) => {
+            console.error(e);
+            res.sendStatus(500);
+        });
 })
 
 module.exports = router
