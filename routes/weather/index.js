5f34abf3e1ec42a3291f55b4e5b8a96d6bc86694
HamzaWaheed <hamza.waheed@hs.fi>
2020-01-31 22:41:59 +0200

    forecast added

--- routes/weather/index.js
+++ routes/weather/index.js
@@ -8,14 +8,14 @@ router.get('/', function (req, res) {
     res.send(msg)
 });
 
-// define the about route
+// weather/currentweather
 router.get('/currentweather', function (req, res) {
     let {
         cords,
         city
     } = req.query;
 
-    let url = `${process.env.OPENWEATHER_URL}&units=metric`;
+    let url = process.env.OPENWEATHER_URL.replace('TYPE', 'weather');
     
     if (cords) {
         cords = cords.split(',');
@@ -35,4 +35,30 @@ router.get('/currentweather', function (req, res) {
         });
 })
 
+// weather/forcast
+router.get('/forecast', function (req, res) {
+    let {
+        cords,
+        city
+    } = req.query;
+
+    let url = process.env.OPENWEATHER_URL.replace('TYPE', 'forecast');
+    
+    if (cords) {
+        cords = cords.split(',');
+        url += '&lat=' + cords[0] + '&lon=' + cords[1];
+    }else if(city){
+        url += '&q=' + city;
+    }else {
+        res.send('Paramters Missing: cords=lat,lon&city=london,uk');
+    }
+    fetch(url).then(response => {
+            return response.json()
+        }).then(response => {
+            res.send(response)
+        }).catch('error', (e) => {
+            console.error(e);
+            res.sendStatus(500);
+        });
+})
 module.exports = router
